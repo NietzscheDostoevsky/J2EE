bb8fdad7053401530f0f079c1677a8a61d6c33b3
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-26 15:41:53 +0530

    jdbc stemps

---
+++ basic-jdbc/src/main/java/com/qsp/JDBCSteps.java
@@ -0,0 +1,15 @@
+package com.qsp;
+
+public class JDBCSteps {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		try {
+			Class.forName("org.postgresql.Driver");
+			System.out.println("driver class is loaded");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
