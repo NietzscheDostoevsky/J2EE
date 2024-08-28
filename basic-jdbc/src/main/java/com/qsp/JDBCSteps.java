e8c20721cde40c32c8d9c78df4ef1d8ae9ff6b2f
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-28 15:43:08 +0530

    jdbs all steps

--- basic-jdbc/src/main/java/com/qsp/JDBCSteps.java
+++ basic-jdbc/src/main/java/com/qsp/JDBCSteps.java
@@ -3,27 +3,37 @@ package com.qsp;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCSteps {
 
 	public static void main(String[] args) throws ClassNotFoundException {
-		try {
-			Class.forName("org.postgresql.Driver");
-			System.out.println("driver class is loaded");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
 		
 		String jdbcUrl = "jdbc:postgresql://localhost:5432/qsp";
         String username = "postgres";
         String password = "m";
+        
 		try {
+			Class.forName("org.postgresql.Driver");
+			System.out.println("driver class is loaded");
+			
 			Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
 			System.out.println("Connection created");
-		} catch (SQLException e) {
 			
+			Statement stm = conn.createStatement(); 
+			System.out.println("statement is created");
+			stm.execute("DELETE FROM student WHERE id=101");
+			stm.execute("INSERT INTO student VALUES (101, 'anuradha')");
+			System.out.println("data inserted");
+			
+			conn.close();
+			System.out.println("Connection close");
+			
+			
+		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
+		
 	}
 }
 
