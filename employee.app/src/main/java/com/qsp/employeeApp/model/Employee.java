76693d382e02be21fdfdf969a06a42533798ece5
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-09-04 15:00:35 +0530

    new project

---
+++ employee.app/src/main/java/com/qsp/employeeApp/model/Employee.java
@@ -0,0 +1,40 @@
+package com.qsp.employeeApp.model;
+
+public class Employee {
+	
+	private int id; 
+	private String name; 
+	private long phone; 
+	private double sal;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public double getSal() {
+		return sal;
+	}
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", phone=" + phone + ", sal=" + sal + "]";
+	}
+	
+	
+
+}
